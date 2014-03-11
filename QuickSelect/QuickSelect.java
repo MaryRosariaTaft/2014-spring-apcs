7e6b13f988439615de18b4227d98071834fc1221
Mary Taft <Mary.Taft97@gmail.com>
2014-03-11 12:33:56 -0400

    Added framework for quick select code.

---
+++ QuickSelect/QuickSelect.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+//runtime: n+(n/2)+(n/4)+(n/8)+... = n+n = 2n = O(n)
+
+public class QuickSelect{
+
+    public static int select(int[] a, int k){
+	
+    }
+
+    public static void partition(int[] a, int[] b, int[] c){
+
+    }
+
+    public static void main(String[] args){
+	int[] a = {8,3,7,12,65,7,33,9,26,542,7,123,0,6}
+	System.out.println(select(a,6)); //6th element (from 0 - infinity) of a
+    }
+
+}
