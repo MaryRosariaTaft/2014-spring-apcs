4f0efe29d13c3a6ae47ae30d27f4af83fd3e0e1c
Mary Taft <Mary.Taft97@gmail.com>
2014-03-12 12:30:53 -0400

    Added classwork.

--- QuickSelect/QuickSelect.java
+++ QuickSelect/QuickSelect.java
@@ -5,17 +5,54 @@ import java.util.*;
 
 public class QuickSelect{
 
-    public static int select(int[] a, int k){
-	
+    private static Random rand = new Random();
+
+    public static int quickselect(int[] L, int k){
+	//(optionally) send quickselect(int[],int,int,int) a COPY of L instead of original to save the original array
+	return quickselect(L,k,0,L.length-1);
     }
 
-    public static void partition(int[] a, int[] b, int[] c){
+    public static int quickselect(int[] L, int k, int left, int right){
+	//call partition
+	//if pivotIndex==k, return
+	//else call quickselect on left OR right side
+	return 0;//temp
+    }
 
+    /*
+
+9,5,(7),11,2,0,14,676,4,77,12,2,4
+
+temp=last/first;
+last/first=L[i];
+L[i]=temp;
+
+     */
+
+    public static int partition(int[] L, int left, int right){
+	//select pivot index in here: random num btwn left and right
+	//changes L
+	//returns partition index
+	int pivotNum = left + rand.nextInt(right-left), frontIndex=left, backIndex=right, temp;
+	for(int i=left;i<right;i++){
+	    if(L[i]<=pivotNum){
+		temp=L[frontIndex];
+		L[frontIndex]=L[i];
+		L[i]=temp;
+		frontIndex++;
+	    }else{
+		temp=L[backIndex];
+		L[backIndex]=L[i];
+		L[i]=temp;
+		backIndex--;
+	    }
+	}
+	return backIndex;
     }
 
     public static void main(String[] args){
-	int[] a = {8,3,7,12,65,7,33,9,26,542,7,123,0,6}
-	System.out.println(select(a,6)); //6th element (from 0 - infinity) of a
+	int[] a = {8,3,7,12,65,7,33,9,26,542,7,123,0,6};
+	System.out.println(partition(a,0,7)); //6th element (from 0 - infinity) of a
     }
 
 }
