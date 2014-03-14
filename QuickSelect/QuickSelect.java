a62da6d5903235b5e826e162ae52957c0b247b08
MaryRosariaTaft <Mary.Taft97@gmail.com>
2014-03-13 22:31:14 -0400

    Partition works, quickselect doesn't, didn't even try quicksort

--- QuickSelect/QuickSelect.java
+++ QuickSelect/QuickSelect.java
@@ -5,13 +5,30 @@ import java.util.*;
 
 public class QuickSelect{
 
+//quick sort
+//get rid of base case
+//new base case: if (right-left)<2
+//get rid of k
+
     private static Random rand = new Random();
+
+    public static void quicksort(int[]L){
+	quicksort(L,0,L.length-1);
+    }
+
+    public static void quicksort(int[]L,int left,int right){
+	if(right-left>=2){	   
+	    int k=partition(L,left,right);
+	    quicksort(L,left,k-1);
+	    quicksort(L,k+1,right);
+	}
+    }
 
     public static int quickselect(int[] L, int k){
 	return quickselect(L,k,0,L.length-1);
     }
 
-    public static int quickselect(int[] L, int k, int left, int right){//both boundaries inclusive //overloading
+    public static int quickselect(int[] L, int k, int left, int right){
 	int pivotIndex=partition(L,left,right);
 	if(pivotIndex==k)
 	    return L[k];
@@ -23,65 +40,31 @@ public class QuickSelect{
 
 
     public static int partition(int[] L, int left, int right){
-
-	//Non-destructive
-	//Apparently doesn't work
-	//separate off elements equal to the pivotNum to avoid duplicate issues--just an optimization
-	int frontIndex=left, backIndex=right, temp=0, pivotInitialIndex=frontIndex+rand.nextInt(backIndex-frontIndex), pivotNum=L[pivotInitialIndex];
+	int frontIndex=left, temp=0, pivotInitialIndex=left+rand.nextInt(right-left), pivotNum=L[pivotInitialIndex];
 	temp=pivotNum;
-	L[pivotInitialIndex]=L[backIndex];
-	L[backIndex]=temp;
-	backIndex--;
+	L[pivotInitialIndex]=L[right];
+	L[right]=temp;
 	for(int i=left;i<right;i++){
-	    if(L[i]<=pivotNum){//so, the pivotNum will reside on the left partitioned piece of the array
-		// temp=L[frontIndex];
-		// L[frontIndex]=L[i];
-		// L[i]=temp;
-		frontIndex++;
-	    }else{
-		temp=L[backIndex];
-		L[backIndex]=L[i];
-		L[i]=temp;
-		backIndex--;
+	    if(L[i]<=pivotNum){
+	    	temp=L[frontIndex];
+	    	L[frontIndex]=L[i];
+	    	L[i]=temp;
+	    	frontIndex++;
 	    }
 	}
 	temp=L[frontIndex];
 	L[frontIndex]=L[right];
 	L[right]=temp;
 	return frontIndex;
-	
-	//throw the pivotNum value at the front or back
-	//try swapping instead
-	//middle is reached when front==back
-
-	// int ans[]=new int[L.length];
-	// int front=left,back=right,pivot=L[front+rand.nextInt(back-front)];//apparently the index comes out to be negative...
-	// for(int i=left;i<right;i++){
-	//     if(L[i]<=pivot){
-	// 	ans[front]=L[i];
-	// 	front++;
-	//     }else{
-	// 	ans[back]=L[i];
-	// 	back--;
-	//     }
-	// }
-	// L=ans;
-	// return front;//last index of the first (smaller) half
-
     }
 
     public static void main(String[] args){
-	int[] a = {9,8,7,6,5,4,3,2,1,0};
-	System.out.println("Original array: "+Arrays.toString(a));
-	System.out.println("Partition index: "+partition(a,0,a.length-1));
-	System.out.println("Partitid array: "+Arrays.toString(a));
-	// System.out.println(quickselect(a,6));
+	int[] a = {7,2,7,9,4,7,7,3,11,44,1,45,7,666,33,9,4,2,2,6,2,8,8888,8,4,2};
+	// int pos=partition(a,0,a.length-1);
+	// System.out.println(pos);
+	// System.out.println(Arrays.toString(a));
+	System.out.println(quickselect(a,6));
    }
 
 }
 
-
-//quick sort
-//get rid of base case
-//new base case: if (right-left)<2
-//get rid of k
