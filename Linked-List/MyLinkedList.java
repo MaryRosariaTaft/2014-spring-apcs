364c69261a2ebaf9c3a40b809186fdc0da9f4ce9
Mary Taft <Mary.Taft97@gmail.com>
2014-03-19 12:03:50 -0400

    Framework for linked list

---
+++ Linked-List/MyLinkedList.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class MyLinkedList{
+
+    private Node head;
+
+    public MyLinkedList(){head=null;}
+
+    public String set(int position, String newString){
+	//change data of node at position to newString
+    }
+
+    public String get(int position){
+	//return string at said position
+    }
+
+    public String toString(){
+
+    }
+
+    public void add(String s, int position){
+	//create node
+	//add to position
+	//throw exception if necessary
+    }
+
+    public void remove(int position){
+	//remore node at position
+    }
+
+    public int find(String s){
+	//return index of first occurrence of s
+	//-1 if it isn't in the List
+    }
+
+    public int length(){
+	//return number of elemetns
+    }
+
+}
