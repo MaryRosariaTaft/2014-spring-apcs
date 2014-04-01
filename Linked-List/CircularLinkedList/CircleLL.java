8d292ab7aa69d92d5509d7b4d75ae7564ad6b0d9
MaryRosariaTaft <Mary.Taft97@gmail.com>
2014-04-01 18:11:19 -0400

    Fixed circular doubly-linked list.

--- Linked-List/CircularLinkedList/CircleLL.java
+++ Linked-List/CircularLinkedList/CircleLL.java
@@ -3,27 +3,58 @@ import java.util.*;
 
 public class CircleLL<E>{
 
-    private  class Node<E>{
+    private class Node<E>{
+
 	private E data;
-	Node<E> next,prev;
+	Node<E> next, prev;
+
 	public Node(E d){
 	    this.data=d;
 	    next=this;
 	    prev=this;
 	}
-	public void setData(E d){data=d;}
-	public E getData(){return data;}
-	public void setNext(Node<E> d){next=d;}
-	public Node<E> getNext(){return next;}
-	public void setPrev(Node<E> d){prev=d;}
-	public Node<E> getPrev(){return prev;}
-	public String toString(){return ""+data;}
+
+	public void setData(E d){
+	    data=d;
+	}
+	public E getData(){
+	    return data;
+	}
+	public void setNext(Node<E> n){
+	    next=n;
+	}
+	public Node<E> getNext(){
+	    return next;
+	}
+	public void setPrev(Node<E> p){
+	    prev=p;
+	}
+	public Node<E> getPrev(){
+	    return prev;
+	}
+
+	public String toString(){
+	    return data.toString();
+	}
+
     }
 
     private Node<E> current;
 
     public void insertBefore(E d){
 	Node<E> n=new Node<E>(d);
+	if(current==null)
+	    current=n;
+	else{
+	    n.setPrev(current.getPrev());
+	    current.getPrev().setNext(n);
+	    n.setNext(current);
+	    current.setPrev(n);
+	    current=n;
+	}
+    }
+
+    public void insertBefore(Node<E> n){
 	if(current==null)
 	    current=n;
 	else{
@@ -48,6 +79,18 @@ public class CircleLL<E>{
 	}
     }
 
+    public void insertAfter(Node<E> n){
+	if(current==null)
+	    current=n;
+	else{
+	    n.setNext(current.getNext());
+	    current.getNext().setPrev(n);
+	    n.setPrev(current);
+	    current.setNext(n);
+	    current=n;
+	}
+    }
+
     public void forward(){
 	current=current.getNext();
     }
@@ -56,17 +99,21 @@ public class CircleLL<E>{
 	current=current.getPrev();
     }
 
-    public E getCurrent(){return current.getData();}
+    public E getCurrent(){
+	if(current==null) return null;
+	return current.getData();
+    }
 
     public String toString(){
-    	if(current==null) return "";
-    	Node<E> temp=current;
-    	String s="";
-    	while(temp!=current){
-    	    s+=temp.getData()+" ";
-    	    temp=temp.getNext();
-    	}
-    	return s;
+	if(current==null)
+	    return "";
+	String s=current.toString()+" ";
+	Node<E> temp=current.getNext();
+	while(temp!=current){
+	    s+=temp.toString()+" ";
+	    temp=temp.getNext();
+	}
+	return s;
     }
 
 }
--- Linked-List/CircularLinkedList/Driver.java
+++ Linked-List/CircularLinkedList/Driver.java
@@ -3,18 +3,27 @@ public class Driver {
     public static void main(String[] args){
 	CircleLL<String> L=new CircleLL<String>();
 	System.out.println(L);
-	L.insertAfter("hello");
-	System.out.println(L);
-	L.insertAfter("world");
-	System.out.println(L.getCurrent());
-	System.out.println(L);
-	L.insertAfter("three");
-	System.out.println(L);
-	System.out.println(L.getCurrent());
+	System.out.println("List: "+L);
+	System.out.println("Current: "+L.getCurrent());
+	System.out.println("Adding before current.");
+	L.insertBefore("hello");
+	System.out.println("List: "+L);
+	System.out.println("Current: "+L.getCurrent());
+	System.out.println("Adding before current.");
+	L.insertBefore("world");
+	System.out.println("List: "+L);
+	System.out.println("Current: "+L.getCurrent());
+	System.out.println("Adding before current.");
+	L.insertBefore("three");
+	System.out.println("List: "+L);
+	System.out.println("Current: "+L.getCurrent());
 	L.back();
-	System.out.println(L.getCurrent());
+	System.out.println("Moved back one.");
+	System.out.println("Current: "+L.getCurrent());
+	System.out.println("Adding after current.");
 	L.insertAfter("inserted");
-	System.out.println(L);
+	System.out.println("Current: "+L.getCurrent());
+	System.out.println("List: "+L);
     }
 
 }
