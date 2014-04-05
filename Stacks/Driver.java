531da51e5097cecbc952672c5de018943e85ff0b
MaryRosariaTaft <Mary.Taft97@gmail.com>
2014-04-05 16:36:20 -0400

    Created simple Stack class

---
+++ Stacks/Driver.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Driver{
+
+    public static void main(String[] args){
+
+	MyStack<Integer> ms = new MyStack<Integer>();
+	ms.push(8);
+	System.out.println(ms);
+	ms.push(8);
+	ms.push(9);
+	ms.push(4);
+	ms.push(10);
+	ms.push(3);
+	ms.push(3);
+	ms.push(3);
+	ms.push(3);
+	ms.push(123);
+	System.out.println(ms);
+	System.out.println(ms.pop());
+	System.out.println(ms);
+	System.out.println("First occurrence of 3: "+ms.search(3));
+	System.out.println("First occurrence of 66: "+ms.search(66));
+
+    }
+
+}
---
+++ Stacks/MyStack.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.util.*;
+
+public class MyStack<E>{
+
+    private class Node<E>{
+	private E data;
+	private Node<E> next;
+	public Node(E data){this.data=data;next=null;}
+	public void setData(E data){this.data=data;}
+	public void setNext(Node<E> next){this.next=next;}
+	public E getData(){return data;}
+	public Node<E> getNext(){return next;}
+	public String toString(){return data.toString();}
+    }
+
+    private Node<E> top;
+
+    public MyStack(){
+	top=null;
+    }
+
+    public boolean empty(){
+	return top==null;
+    }
+
+    public E peek(){
+	if(empty())
+	    throw new EmptyStackException();
+	return top.getData();
+    }
+
+    public E pop(){
+	E temp=peek();
+	top=top.getNext();
+	return temp;
+    }
+
+    public void push(E d){
+	Node<E> temp=new Node<E>(d);
+	temp.setNext(top);
+	top=temp;
+    }
+
+    public int search(Object o){
+	int pos=1;
+	Node<E> current=top;
+	while(current!=null){
+	    if(current.getData()==o)
+		return pos;
+	    pos++;
+	    current=current.getNext();
+	}
+	return -1;
+    }
+
+    public String toString(){
+	String s="";
+	Node<E> current=top;
+	while(current!=null){
+	    s=current.toString()+" "+s;
+	    current=current.getNext();
+	}
+	return s;
+    }
+
+}
