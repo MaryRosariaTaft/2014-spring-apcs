4111a0ff897610f798194b0e3b92ebd433732804
MaryRosariaTaft <Mary.Taft97@gmail.com>
2014-04-30 23:34:11 -0400

    Sloppy framework for binary tree

---
+++ Trees/BinarySearchTree.java
@@ -0,0 +1,27 @@
+public class BinarySearchTree{
+
+    private class Node{
+
+	private int value, count;
+	private Node left, right; //children
+
+    }
+
+    private Node root;
+
+    /*
+current=root;
+while(current!=null)
+if (target<current.value)
+current=current.getLeft()
+>
+=.getRight
+if(==)
+return [[find]] OR increment[[insert]]
+
+toString
+recursion, print out left <-- this --> right, and if anything == null print ' ' (a space)
+
+     */
+
+}
