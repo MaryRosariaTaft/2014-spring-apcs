2ede4f593cda88449a9cb007a063cec42d3dcc48
MaryRosariaTaft <Mary.Taft97@gmail.com>
2014-05-07 18:28:37 -0400

    Added driver with Tree named Fred

---
+++ Trees/Driver.java
@@ -0,0 +1,16 @@
+public class Driver{
+
+    public static void main(String[] args){
+	BinarySearchTree fred = new BinarySearchTree();
+	fred.insert(50);
+	fred.insert(25);
+	fred.insert(75);
+	fred.insert(12);
+	fred.insert(37);
+	fred.insert(87);
+	fred.insert(63);
+	System.out.println(fred);
+    }
+
+
+}
