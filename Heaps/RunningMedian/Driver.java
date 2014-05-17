00353bbe7dacc96ac94c324f9f6a42cd23f23413
MaryRosariaTaft <Mary.Taft97@gmail.com>
2014-05-17 17:00:27 -0400

    I'm pretty sure everything works.  Pretty sure.

--- Heaps/RunningMedian/Driver.java
+++ Heaps/RunningMedian/Driver.java
@@ -4,90 +4,60 @@ import java.util.*;
 public class Driver{
 
     public static void main(String[] args){
-	Random rand=new Random();
+	int size=(int)(Integer.parseInt(args[0]));
+	for(int seed=1;seed<args.length;seed++){
+	    Random R=new Random((int)(Integer.parseInt(args[seed])));
+
+	    System.out.println("Test 1");
+	    System.out.println();
+	    test1(R,size);
+
+	    System.out.println("__________________________");
+	    System.out.println();
+	    System.out.println("Test 2");
+	    System.out.println();
+	    test2(R,size);
+	}
+    }
+
+    public static void test1(Random R, int size){
+
 	RunningMedian rm=new RunningMedian();
 
 	//put values in heaps
-	for(int i=0;i<50;i++){
-	    int temp=rand.nextInt(100)+1;
+	for(int i=0;i<size;i++){
+	    int temp=R.nextInt(100)+1;
 	    rm.add(temp);
-	    System.out.println("Adding "+temp);
+	    System.out.print("Added "+temp+"; ");
+	    System.out.println("median is now "+rm.median()+".");
 	    System.out.println(rm);
 	}
 
-	//print out arrays
-	int[] max=rm.getMax();
-	int[] min=rm.getMin();
-	System.out.println("Array1: ");
-	for(int i=0;i<27;i++){
-	    System.out.print(max[i]+",");
+    }
+
+    public static void test2(Random R, int size){
+
+	RunningMedian rm=new RunningMedian();
+
+	for(int i=0;i<size;i++){
+
+	    //put values in heaps
+	    int temp=R.nextInt(100)+1;
+	    rm.add(temp);
+	    System.out.print("Added "+temp+"; ");
+	    System.out.println("median is now "+rm.median()+".");
+	    System.out.println(rm);
+
+	    //20% chance of removing value
+	    if(Math.random()<0.2){
+		rm.remove();
+		System.out.println("Removed median(s).");
+		System.out.println(rm);
+	    }
+
 	}
-	System.out.println();
-	System.out.println("Array2: ");
-	for(int i=0;i<27;i++){
-	    System.out.print(min[i]+",");
-	}
-	System.out.println();
-
-	System.out.println();
-
-	//check for median
-	System.out.println("Median: "+rm.median());
-
-	System.out.println();
-
-	//remove median & check for consistency with above number
-	System.out.println("Remove: "+rm.remove());
-
-	System.out.println();
-
-	System.out.println("Testing grow and shrink");
-	System.out.println("Original length: "+rm.getMax().length);
-	rm.grow();
-	System.out.println("Length after grow: "+rm.getMax().length);
-	rm.shrink();
-	System.out.println("Length after shrink: "+rm.getMax().length);
-
-	System.out.println();
-
-	//print arrays to see if it worked
-	max=rm.getMax();
-	min=rm.getMin();
-	System.out.println("Array1: ");
-	for(int i=0;i<27;i++){
-	    System.out.print(max[i]+",");
-	}
-	System.out.println();
-	System.out.println("Array2: ");
-	for(int i=0;i<27;i++){
-	    System.out.print(min[i]+",");
-	}
-	System.out.println();
-
-	//emptying container
-	int size=rm.maxSize();
-	while(size>0){
-	    rm.remove();
-	    size=rm.maxSize();
-	}
-
-	//print arrays to see if it worked
-	max=rm.getMax();
-	min=rm.getMin();
-	System.out.println("Array1: ");
-	for(int i=0;i<27;i++){
-	    System.out.print(max[i]+",");
-	}
-	System.out.println();
-	System.out.println("Array2: ");
-	for(int i=0;i<27;i++){
-	    System.out.print(min[i]+",");
-	}
-	System.out.println();
-
-	//make sure exception is thrown appropriately
-	System.out.println(rm.median());
 
     }
+
 
 }
--- Heaps/RunningMedian/RunningMedian.java
+++ Heaps/RunningMedian/RunningMedian.java
@@ -79,160 +79,160 @@ public class RunningMedian{
 	}
     }
 
-	public void pushDown(int[] a, int index, int upperBound, String type){
-	    if(type.equals("max")){
-		//while the index has at least one child (the left child)
-		while(index*2+1<upperBound){
-		    //assign the child as the left child
-		    int child=index*2+1;
-		    //make a variable to store which index the given index will potentially be swapped with
-		    int swapIndex=index;
-		    //if the left child is greater than the parent (the value at the given index)
-		    if(a[swapIndex]<a[child])
-			swapIndex=child;
-		    //if the right child exists
-		    if(child+1<upperBound)
-			//if it's greater than the left child (since the parent should be swapped with the LARGER of the two children)
-			if(a[swapIndex]<a[child+1])
-			    swapIndex=child+1;
-		    //swap the value at the given index with the proper child if necessary
-		    if(index!=swapIndex){
-			swap(a,index,swapIndex);
-			index=swapIndex;
-		    }
-		    //value at index is in the correct place
-		    else
-			break;
+    public void pushDown(int[] a, int index, int upperBound, String type){
+	if(type.equals("max")){
+	    //while the index has at least one child (the left child)
+	    while(index*2+1<upperBound){
+		//assign the child as the left child
+		int child=index*2+1;
+		//make a variable to store which index the given index will potentially be swapped with
+		int swapIndex=index;
+		//if the left child is greater than the parent (the value at the given index)
+		if(a[swapIndex]<a[child])
+		    swapIndex=child;
+		//if the right child exists
+		if(child+1<upperBound)
+		    //if it's greater than the left child (since the parent should be swapped with the LARGER of the two children)
+		    if(a[swapIndex]<a[child+1])
+			swapIndex=child+1;
+		//swap the value at the given index with the proper child if necessary
+		if(index!=swapIndex){
+		    swap(a,index,swapIndex);
+		    index=swapIndex;
 		}
-	    }else if(type.equals("min")){
-		//while the index has at least one child (the left child)
-		while(index*2+1<upperBound){
-		    //assign the child as the left child
-		    int child=index*2+1;
-		    //make a variable to store which index the given index will potentially be swapped with
-		    int swapIndex=index;
-		    //if the left child is greater than the parent (the value at the given index)
-		    if(a[swapIndex]>a[child])
-			swapIndex=child;
-		    //if the right child exists
-		    if(child+1<upperBound)
-			//if it's greater than the left child (since the parent should be swapped with the LARGER of the two children)
-			if(a[swapIndex]>a[child+1])
-			    swapIndex=child+1;
-		    //swap the value at the given index with the proper child if necessary
-		    if(index!=swapIndex){
-			swap(a,index,swapIndex);
-			index=swapIndex;
-		    }
-		    //value at index is in the correct place
-		    else
-			break;
+		//value at index is in the correct place
+		else
+		    break;
+	    }
+	}else if(type.equals("min")){
+	    //while the index has at least one child (the left child)
+	    while(index*2+1<upperBound){
+		//assign the child as the left child
+		int child=index*2+1;
+		//make a variable to store which index the given index will potentially be swapped with
+		int swapIndex=index;
+		//if the left child is greater than the parent (the value at the given index)
+		if(a[swapIndex]>a[child])
+		    swapIndex=child;
+		//if the right child exists
+		if(child+1<upperBound)
+		    //if it's greater than the left child (since the parent should be swapped with the LARGER of the two children)
+		    if(a[swapIndex]>a[child+1])
+			swapIndex=child+1;
+		//swap the value at the given index with the proper child if necessary
+		if(index!=swapIndex){
+		    swap(a,index,swapIndex);
+		    index=swapIndex;
 		}
+		//value at index is in the correct place
+		else
+		    break;
 	    }
 	}
+    }
 
-	public void swap(int[] a, int x, int y){
-	    int temp=a[x];
-	    a[x]=a[y];
-	    a[y]=temp;
+    public void swap(int[] a, int x, int y){
+	int temp=a[x];
+	a[x]=a[y];
+	a[y]=temp;
+    }
+
+    public void removeRoot(int[] a, int upperBound, String type){
+	//move bottom-right leaf to root position
+	a[0]=a[upperBound-1];
+	//clear out bottom-right (for cleanness)
+	a[upperBound-1]=0;
+	//push down the displaced value into its correct position
+	pushDown(a,0,upperBound-1,type);
+    }
+
+    public void add(int value){
+
+	fixSize();
+
+	//if adding first element
+	if(maxSize==0){
+	    max[0]=value;
+	    maxSize=1;
+	    return;
 	}
-
-	public void removeRoot(int[] a, int upperBound, String type){
-	    //move bottom-right leaf to root position
-	    a[0]=a[upperBound-1];
-	    //clear out bottom-right (for cleanness)
-	    a[upperBound-1]=0;
-	    //push down the displaced value into its correct position
-	    pushDown(a,0,upperBound-1,type);
+	//if adding second element
+	if(minSize==0){
+	    min[0]=value;
+	    minSize=1;
+	    return;
 	}
+	//else, do all the fancy stuff:
 
-	public void add(int value){
-
-	    fixSize();
-
-	    //if adding first element
-	    if(maxSize==0){
-		max[0]=value;
-		maxSize=1;
-		return;
-	    }
-	    //if adding second element
-	    if(minSize==0){
-		min[0]=value;
-		minSize=1;
-		return;
-	    }
-	    //else, do all the fancy stuff:
-
-	    if(value<=median()){
-		//add to max heap
-		max[maxSize]=value;
-		//push up to right place:
-		pushUp(max,maxSize,"max");
-		//increment maxSize for the following check (rebalancing)
-		maxSize++;
-		//rebalance* if necessary
-		//*rebalance by moving root of max heap into the min heap
-		if(maxSize>minSize+1){
-		    //move max's root into min
-		    min[minSize]=max[0];
-		    pushUp(min,minSize,"min");
-		    minSize++;
-		    //reconfigure max heap
-		    removeRoot(max,maxSize,"max");
-		    maxSize--;
-		}
-	    }else{//if value>median()
-		//add to min heap
-		min[minSize]=value;
-		//push up to right place:
+	if(value<=median()){
+	    //add to max heap
+	    max[maxSize]=value;
+	    //push up to right place:
+	    pushUp(max,maxSize,"max");
+	    //increment maxSize for the following check (rebalancing)
+	    maxSize++;
+	    //rebalance* if necessary
+	    //*rebalance by moving root of max heap into the min heap
+	    if(maxSize>minSize+1){
+		//move max's root into min
+		min[minSize]=max[0];
 		pushUp(min,minSize,"min");
-		//increment...
 		minSize++;
-		//rebalance if necessary
-		if(minSize>maxSize){
-		    //move min's root into max
-		    max[maxSize]=min[0];
-		    pushUp(max,maxSize,"max");
-		    maxSize++;
-		    //reconfigure min heap
-		    removeRoot(min,minSize,"min");
-		    minSize--;
-		}
+		//reconfigure max heap
+		removeRoot(max,maxSize,"max");
+		maxSize--;
 	    }
-
-	}
-
-	//removes median(s)
-	public double remove(){
-
-	    fixSize();
-
-	    if(empty()){
-		throw new IllegalStateException();
-	    }
-  
-	    double temp=median();
-	    //remove root from min heap if median is the average of both roots
-	    if(maxSize==minSize){
+	}else{//if value>median()
+	    //add to min heap
+	    min[minSize]=value;
+	    //push up to right place:
+	    pushUp(min,minSize,"min");
+	    //increment...
+	    minSize++;
+	    //rebalance if necessary
+	    if(minSize>maxSize){
+		//move min's root into max
+		max[maxSize]=min[0];
+		pushUp(max,maxSize,"max");
+		maxSize++;
+		//reconfigure min heap
 		removeRoot(min,minSize,"min");
 		minSize--;
 	    }
-	    //remove root from max heap no matter what
-	    removeRoot(max,maxSize,"max");
-	    maxSize--;
-	    return temp;
 	}
 
-	//average if sizes are equal, root of max otherwise
-	public double median(){
-	    if(empty()){
-		throw new IllegalStateException();
-	    }
-	    if(maxSize==minSize)
-		return (double)(max[0]+min[0])/2;
-	    return max[0];
+    }
+
+    //removes median(s)
+    public double remove(){
+
+	fixSize();
+
+	if(empty()){
+	    throw new IllegalStateException();
 	}
+  
+	double temp=median();
+	//remove root from min heap if median is the average of both roots
+	if(maxSize==minSize){
+	    removeRoot(min,minSize,"min");
+	    minSize--;
+	}
+	//remove root from max heap no matter what
+	removeRoot(max,maxSize,"max");
+	maxSize--;
+	return temp;
+    }
+
+    //average if sizes are equal, root of max otherwise
+    public double median(){
+	if(empty()){
+	    throw new IllegalStateException();
+	}
+	if(maxSize==minSize)
+	    return (double)(max[0]+min[0])/2;
+	return max[0];
+    }
 
     public String toString(){
 	String ans="";
